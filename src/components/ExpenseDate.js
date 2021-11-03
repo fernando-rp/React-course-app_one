ca2765b160edafb151e00cde8adc9c88241a8bbb
Fernando <fernandor.p@hotmail.com>
2021-11-03 01:31:30 -0300

    task 1 done

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,19 @@
+import './ExpenseDate.css'
+
+function ExpenseDate(props){
+
+    const month= props.date.toLocaleString('es-CL',{month:'long'});
+    const year= props.date.getFullYear();
+    const day= props.date.toLocaleString('es-CL',{day:'2-digit'});
+
+    return(
+        <div className="expense-date" >
+            <div className="expense-date__month">{month}</div>
+            <div className="expense-date__year">{year}</div>
+            <div className="expense-date__day">{day} </div>
+        </div>
+    )
+
+}
+
+export default ExpenseDate;
